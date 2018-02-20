6477d1c720bd28ca247dcb9fd1b36bc3d0a8dd1c
Vinaum8 <vinifernandes_11@hotmail.com>
2018-02-20 13:25:28 -0300

    Primeiros Passos com Django

---
+++ polls/views.py
@@ -0,0 +1,7 @@
+import django.shortcuts
+
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
